52c47f884d5918f2b4fd8dff435d9c63353ae589
jak010 <bluetoon@naver.com>
2024-09-14 21:13:10 +0900

    deployment update

--- deployment/application.py
+++ deployment/application.py
@@ -1,11 +1,12 @@
 import os
 
-import requests
 from dotenv import load_dotenv
 
-from libs.github.application import GithubAPI
+from libs.github.github_api import GithubAPI
+from libs.http_requestor import HttpRequestProxy
 from libs.mkdown.parser import MarkDownParser
-from libs.tistory.tistory import TistoryUploader
+from libs.tistory.tistory_rss_parser import TistoryRssParser
+from libs.tistory.tistory_uploader import TistoryUploader
 
 load_dotenv()
 
@@ -14,21 +15,28 @@ gh = GithubAPI(
     repo_name=os.environ['GH_REPO']
 )
 
-sha = gh.repository.get_latest_sha_on_commit()
-addition_files = gh.repository.get_added_file_by_latest_commits(sha)
+latest_commit_sha = gh.repository.get_latest_commit_history()
+addition_markdown_files = gh.repository.get_latest_commit_history_with_added_file_and_ext(sha=latest_commit_sha, file_ext="md")
 
-if len(addition_files) <= 1:
-    read_url = addition_files[0]["raw_url"]
+markdowns = [
+    MarkDownParser(
+        HttpRequestProxy.get(addition_markdown_file.raw_url)
+    )
+    for addition_markdown_file in addition_markdown_files
+]
 
-    if read_url:
-        r = requests.get(read_url)
+# Write on Blog Post
+tistory_rss_parser = TistoryRssParser(
+    HttpRequestProxy.get(f"https://{os.environ['TISTORY_BLOG_DOMAIN']}.tistory.com/rss")
+)
 
-        markdown_parser = MarkDownParser(r.text)
+uploader = TistoryUploader(
+    tsession=os.environ["TSESSION"],
+    domain=os.environ["TISTORY_BLOG_DOMAIN"],
+)
 
-        TistoryUploader(
-            tsession=os.environ["TSESSION"],
-            domain=os.environ["TISTORY_BLOG_DOMAIN"],
-        ).execute(
-            title=markdown_parser.get_markdown_title(),
-            content=markdown_parser.get_markdown_to_html
-        )
+for markdown in tistory_rss_parser.uploadable_markdowns(markdown_parsers=markdowns):
+    uploader.execute(
+        title=markdown.get_markdown_title(),
+        content=markdown.get_markdown_to_html
+    )
